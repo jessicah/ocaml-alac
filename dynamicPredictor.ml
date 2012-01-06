6c5fd8e3cd1b0c6d2c398a3a5deaf73b20e4f20c
jessicah <jessica.l.hamilton@gmail.com>
2012-01-07 10:59:14 +1300

    Change shift_right_logical to shift_right (arithmetic).

--- dynamicPredictor.ml
+++ dynamicPredictor.ml
@@ -85,10 +85,8 @@ for (j = lim; j < num; j++)
 *)
 
 let sign_of_int i =
-	(*let negishift = (-i) lsr 31 in
-	negishift lor (i lsr 31)*)
-	let negishift = Int32.shift_right_logical (Int32.neg i) 31 in
-	Int32.logor negishift (Int32.shift_right_logical i 31)
+let negishift = Int32.shift_right (Int32.neg i) 31 in
+	Int32.logor negishift (Int32.shift_right i 31)
 
 let rec loop_while p f = function
 | n when n >= 0 -> if p (f n) then loop_while p f (n-1)
@@ -107,13 +105,13 @@ let unpc_block (pc1 : int32a) (out : int32a) num (coefs : int16a) numactive chan
 		let prev = ref out.{0} in
 		for j = 1 to num - 1 do
 			let del = Int32.add pc1.{j} !prev in
-			prev := Int32.shift_right_logical (Int32.shift_left del chanshift) chanshift;
+			prev := Int32.shift_right (Int32.shift_left del chanshift) chanshift;
 			out.{j} <- !prev;
 		done
 	end else begin
 		for j = 1 to numactive do
 			let del = Int32.add pc1.{j} out.{j-1} in
-			out.{j} <- Int32.shift_right_logical (Int32.shift_left del chanshift) chanshift;
+			out.{j} <- Int32.shift_right (Int32.shift_left del chanshift) chanshift;
 		done;
 
 		let lim = numactive + 1 in
@@ -129,22 +127,22 @@ let unpc_block (pc1 : int32a) (out : int32a) num (coefs : int16a) numactive chan
 			let del = pc1.{j} in
 			let del0 = ref pc1.{j} in
 			let sg = sign_of_int del in
-			let del = Int32.add del (Int32.add top (Int32.shift_right_logical (Int32.add !sum1 (Int32.of_int denhalf)) denshift)) in
-			out.{j} <- Int32.shift_right_logical (Int32.shift_left del chanshift) chanshift;
+			let del = Int32.add del (Int32.add top (Int32.shift_right (Int32.add !sum1 (Int32.of_int denhalf)) denshift)) in
+			out.{j} <- Int32.shift_right (Int32.shift_left del chanshift) chanshift;
 
 			if sg > 0l then begin
 				loop_while (fun _ -> !del0 > 0l) (fun k ->
 					let dd = Int32.sub top out.{j-1-k} in
 					let sgn = sign_of_int dd in
 					coefs.{k} <- coefs.{k} - (Int32.to_int sgn);
-					del0 := Int32.sub !del0 (Int32.mul (Int32.of_int (numactive - k)) (Int32.shift_right_logical (Int32.mul sgn dd) denshift));
+					del0 := Int32.sub !del0 (Int32.mul (Int32.of_int (numactive - k)) (Int32.shift_right (Int32.mul sgn dd) denshift));
 					) (numactive - 1);
 			end else if sg < 0l then begin
 				loop_while (fun _ -> !del0 < 0l) (fun k ->
 					let dd = Int32.sub top out.{j-1-k} in
 					let sgn = sign_of_int dd in
 					coefs.{k} <- coefs.{k} + (Int32.to_int sgn);
-					del0 := Int32.sub !del0 (Int32.mul (Int32.of_int (numactive - k)) (Int32.shift_right_logical (Int32.mul (Int32.neg sgn) dd) denshift));
+					del0 := Int32.sub !del0 (Int32.mul (Int32.of_int (numactive - k)) (Int32.shift_right (Int32.mul (Int32.neg sgn) dd) denshift));
 					) (numactive - 1);
 			end;
 		done;
