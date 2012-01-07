4a29e7db5513df6aae48bc9ee008514fa6d4146d
jessicah <jessica.l.hamilton@gmail.com>
2012-01-07 13:18:22 +1300

    Conversion utilities for bigarrays

---
+++ bigarrayUtils.ml
@@ -0,0 +1,21 @@
+
+(* BigarrayUtils : Conversion between different array types *)
+
+open Bigarray
+
+(* this creates a copy of the data =/ *)
+
+let of_string string =
+	let ba = Array1.create char c_layout (String.length string) in
+	for i = 0 to String.length string - 1 do
+		Array1.unsafe_set ba i string.[i];
+	done;
+	ba
+
+external change_flags : ('a, 'b, c_layout) Array1.t -> ('c, 'd) kind -> int -> ('c, 'd, c_layout) Array1.t = "caml_ba_change_flags"
+
+let int32_from_string string : ArrayTypes.int32a =
+	change_flags (of_string string) int32 (String.length string / 4)
+
+let int32_to_uint16 (ba : ArrayTypes.int32a) : ArrayTypes.uint16a =
+	change_flags ba int16_unsigned (Array1.dim ba * 2)
